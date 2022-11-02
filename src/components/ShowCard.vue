7ce9db89e444fa6ccbb3399f2252372c3fee10d7
LeeChiYun <carrick035@gmail.com>
2022-11-02 16:08:11 +0900

    모달에 상품 동적으로 바인딩하기 위해 data.js 만듬 (title, image, id), ShowCard : display = none 선언

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,19 +1,18 @@
 <template>
-  <v-row justify="center">
+  <v-row class="modalClass" justify="center">
     <v-dialog v-model="dialog" persistent max-width="450">
-      <template v-slot:activator="{ on, attrs }">
+      <template v-if="false" v-slot:activator="{ on, attrs }">
         <v-btn @click="showUp()" color="primary" dark v-bind="attrs" v-on="on">
           Open Dialog
         </v-btn>
       </template>
       <v-card>
         <v-card-title class="item-text">
-          ""님 ""당첨 축하드립니다!
+          ""님 {{ which }}당첨 축하드립니다!
+          <!-- 당첨된 것 data[i].title -->
         </v-card-title>
         <v-img src="@/assets/000.jpg" class="item-img"></v-img>
-        <!-- 룰렛 결과 데이터에 따라 
-        여기 v-img src에 바인딩되는 assets (이미지)만 바뀔 것
-        -->
+        <!-- 당첨된 것 data[i].image -->
 
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -32,10 +31,13 @@
 </template>
 
 <script>
+import data from "../data.js";
+
 export default {
   data() {
     return {
       dialog: false,
+      item: data,
     };
   },
   methods: {
@@ -43,17 +45,31 @@ export default {
       console.log(`모달창 열리는 함수`);
       console.log(`showup으로 넘어오는 결과 값 : ${which}`);
       this.dialog = true;
+
+      console.log(
+        `data.js에서 가져온 것 띄우기 ${data} ${data[0].title} ${data[0].image}`
+      );
+
+      // which render중에 접근되었으나, 인스턴트에 define 되지 않았음 life-cycle issue
     },
   },
 };
 </script>
 
 <style>
+.v-overlay {
+  z-index: 9999;
+}
+
 .v-card {
   text-align: center;
 }
 
 .item-text {
   font-size: 18px;
+}
+
+.modalClass {
+  z-index: 9999;
 }
 </style>
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 0,
+    title: "10만원",
+    image: "@/assets/000.jpg",
+  },
+  {
+    id: 1,
+    title: "꽝",
+    image: "https://codingapple1.github.io/vue/room1.jpg",
+  },
+  {
+    id: 2,
+    title: "박카스",
+    image: "@/assets/005.jpg",
+  },
+  {
+    id: 3,
+    title: "5천원",
+    image: "@/assets/003.jpg",
+  },
+  {
+    id: 4,
+    title: "5만원",
+    image: "@/assets/001.jpg",
+  },
+  {
+    id: 5,
+    title: "꽝",
+    image: "@/assets/꽝.jpg",
+  },
+  {
+    id: 5,
+    title: "3만원",
+    image: "@/assets/002.jpg",
+  },
+  {
+    id: 5,
+    title: "1만원",
+    image: "@/assets/001.jpg",
+  },
+];
+
+export default data;
