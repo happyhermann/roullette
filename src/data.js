d2b713bf83023678e91d4b0d4f3a270bdb39e2c9
LeeChiYun <carrick035@gmail.com>
2022-11-03 09:49:02 +0900

    vuejs 로컬 호스트 동적 src 이미지 바인딩 이슈 해결 *require

--- src/data.js
+++ src/data.js
@@ -2,42 +2,42 @@ const data = [
   {
     id: 0,
     title: "10만원",
-    image: "@/assets/000.jpg",
+    image: require("./assets/000.jpg"),
   },
   {
     id: 1,
     title: "꽝",
-    image: "https://codingapple1.github.io/vue/room1.jpg",
+    image: require("./assets/꽝.png"),
   },
   {
     id: 2,
     title: "박카스",
-    image: "@/assets/005.jpg",
+    image: require("./assets/005.jpg"),
   },
   {
     id: 3,
     title: "5천원",
-    image: "@/assets/003.jpg",
+    image: require("./assets/004.jpg"),
   },
   {
     id: 4,
     title: "5만원",
-    image: "@/assets/001.jpg",
+    image: require("./assets/001.jpg"),
   },
   {
     id: 5,
     title: "꽝",
-    image: "@/assets/꽝.jpg",
+    image: require("./assets/꽝.png"),
   },
   {
     id: 5,
     title: "3만원",
-    image: "@/assets/002.jpg",
+    image: require("./assets/002.jpg"),
   },
   {
     id: 5,
     title: "1만원",
-    image: "@/assets/001.jpg",
+    image: require("./assets/001.jpg"),
   },
 ];
 
