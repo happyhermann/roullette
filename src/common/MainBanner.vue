84fd6977c7c963c05724b4d6d63b17bc6f1670c8
LeeChiYun <carrick035@gmail.com>
2022-11-04 17:46:11 +0900

    배너 컴포넌트 setCookie 생성

--- src/common/MainBanner.vue
+++ src/common/MainBanner.vue
@@ -2,10 +2,24 @@
   <div class="container">
     <a @click="openModal()" class="btn js-click-modal">Open Modal</a>
     <div class="modal">
-      <div class="header">This is a full-width modal with a title</div>
       <div class="body">
-        <p>And here is all its contents.</p>
-        <a @click="closeModal()" class="btn js-close-modal">Close</a>
+        <article class="ad-img-box">
+          <img
+            class="ad-img"
+            src="https://storage.cobak.co/uploads/1635821281397702_2600bbdbd5.png"
+            alt="배너 이미지"
+          />
+          <!-- 홍보할 컨텐츠 이미지 -->
+        </article>
+      </div>
+      <div class="footer">
+        <span @click="closeModal()" class="btn js-temp-close-modal"
+          >다시보지 않기</span
+        >
+        <!-- 다시보지 않기 버튼  -->
+
+        <span @click="closeModal()" class="btn js-close-modal">닫기</span>
+        <!-- 단순 닫기 버튼 -->
       </div>
     </div>
   </div>
@@ -20,6 +34,40 @@ export default {
     //
   }),
   methods: {
+    // 설정 기간 동안 팝업 안보이기
+
+    // setCookie로 쿠키이름, 유효시간을 쿠키에 저장
+    // 해당 일자가 도달하면 쿠키를 자동으로 삭제
+    setCookie: function (name, value, expiredays) {
+      var today = new Date(); // date 객체 생성 후 변수에 저장
+      console.log(today.getDate());
+      today.setDate(today.getDate() + expiredays);
+      // set time (현재시간 + 지정시간)
+
+      document.cookie =
+        name + "=" + escape(value) + "; expires=" + today.toGMTString();
+
+      // 위 정보를 cookie에 burning
+    },
+    // 쿠키 가져오는 함수
+    getCookie: function (name) {
+      console.log(name);
+      var cookie = document.cookie;
+      if (document.cookie != "") {
+        var cookie_array = cookie.split(";");
+        console.log(cookie_array);
+        for (var index in cookie_array) {
+          var cookie_name = cookie_array[index].split("=");
+          if (cookie_name[0] == "mycookie") {
+            return cookie_name[1];
+          }
+        }
+      }
+      return;
+    },
+
+    // getCookie를 통해 저장된 쿠키 값을 가져옴
+
     openModal: function () {
       console.log(`오픈 모달`);
       const container = document.querySelector(".container");
@@ -34,16 +82,30 @@ export default {
       console.log(`모달 닫기 `);
     },
   },
+
+  created() {
+    console.log(`====created====`);
+    this.setCookie("mycookie", "popupEnd", 1);
+    // 쿠키 세팅 함수 호출
+  },
+  beforeMount() {
+    console.log(`====beforeMount====`);
+    this.getCookie("mycookie");
+    // 마운트 이전에 쿠키 값 있는지 확인
+  },
 };
 </script>
 
 <style lang="scss">
+/* 최상위 부모 container 스타일 */
 .container {
+  width: 100%;
   color: #333;
   font-family: Lato;
   font-weight: 300;
   text-align: center;
   background: #333;
+  z-index: 10999;
 }
 .container > btn {
   margin: 60px;
@@ -51,10 +113,13 @@ export default {
   font-size: 30px;
   border-radius: 40px;
 }
+
+/* 모달창 메인 (보이고 안보이게 하는 부분) */
 .modal {
   background-color: #fff;
+  border-radius: 20px;
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
   margin: 0;
   padding: 0;
   transition: all 600ms cubic-bezier(0.86, 0, 0.07, 1);
@@ -62,34 +127,52 @@ export default {
   position: fixed;
   left: 0;
   text-align: left;
-  .header {
-    padding: 20px;
-    border-bottom: 1px solid #ddd;
-    font: 300 24px Lato;
-    position: relative;
-  }
+
+  /* body / article 메인 컨텐츠 스타일  */
   .body {
-    padding: 20px;
+    width: 100%;
     font: 300 16px Lato;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
   }
 }
 
-.btn {
-  background: linear-gradient(to right, rgb(220, 227, 91), rgb(69, 182, 73));
-  padding: 10px 25px;
-  display: inline-block;
-  border-radius: 25px;
-  margin: 20px 0;
-  color: #fff;
-  position: relative;
-  transition: all 0.4s ease;
-  cursor: pointer;
-  &:hover {
-    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.2);
+article {
+  img {
+    width: 100%;
+    height: 100%;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
   }
 }
 
 .container.modal-open .modal {
   top: 0;
+}
+
+// 닫기 버튼 CSS
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 10px 10px 35px 10px;
+
+  span {
+    padding: 5px 25px;
+  }
+  span:first-child {
+    font-weight: 400;
+  }
+}
+
+.btn {
+  font-size: 15px;
+  font-weight: 700;
+  display: inline-block;
+  color: #000;
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
